bc6edf8fea170d20ddc62ed48240904c519c1d19
jinghui.zhang <15671678163@163.com>
2020-05-12 15:21:33 +0800

    add a answer branch and upload answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import urllib.request as request
 import time
 
 from pandas import DataFrame
@@ -17,13 +16,18 @@ class SimpleWordCountExample:
         self.get_current_time_func = lambda: int(round(time.time() * 1000))
 
     def load_source_file(self) -> DataFrame:
-        pass
+        return pd.read_fwf(self.source_url)
 
     def do_word_count(self, df: DataFrame) -> DataFrame:
-        pass
+        return df.iloc[:, 0].str \
+            .extractall(pat=r"(\b[a-zA-Z0-9]+\b)")[0] \
+            .value_counts() \
+            .rename_axis('word') \
+            .reset_index(name='count')
 
     def persist_result(self, df: DataFrame):
-        pass
+        df['start_with'] = df['word'].map(lambda x: np.where(x.isalpha(), x.capitalize()[0:1], "others"))
+        df.to_parquet("word_count_result", partition_cols=['start_with'], compression=None)
 
     def run(self):
         raw_df = self.load_source_file()
